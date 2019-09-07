00c822c0f1bee5841b293c3c1eebe87374ad0f51
anarhuseynov <ahuseynov1995@gmail.com>
2019-09-08 00:30:40 +0400

    wrote request class and import index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log("Test");
+import {Request} from './request';
+
+
+// choose elements
+const form = document.querySelector('#employee-form');
+const nameInput = document.querySelector('#name');
+const department = document.querySelector('#department');
+const salary = document.querySelector('#salary');
+const employeesList = document.querySelector("#employees");
+const update = document.querySelector("#update");
+
+
+const request = new Request("http://localhost:3000/employees");
+
+// request.get()
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
+
+
+// request.post({
+//     name: "Farid Abasov",
+//     department: "IT",
+//     salary: 6000
+// })
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
+
+
+// request.put(1,{
+//     name: "Anar Huseynov",
+//     department: "IT",
+//     salary: 2000
+// })
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
+
+// request.delete(3)
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
